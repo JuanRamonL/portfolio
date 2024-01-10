2ab2f74b95ff66f8ee0c216e36f348580441e6e3
JuanRamonL <78512252+JuanRamonL@users.noreply.github.com>
2024-01-10 14:07:11 -0300

    Add skills

--- my-project/src/Components/Skills.jsx
+++ my-project/src/Components/Skills.jsx
@@ -2,9 +2,83 @@ function Contact() {
     return (
       <>
         <div className="bg-rose-600">
-          <h1 className="text-3xl font-bold underline">
-          Habilidades
-          </h1>
+          <div class="bg-white py-14">
+            <h2 class="text-2xl tracking-widest text-gray-600 text-center">Habilidades</h2>
+            <h3 class="mt-8 text-center text-5xl text-gray-600 font-bold">Lenguajes de programación</h3>
+
+            <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+              
+              <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
+                <div class="w-sm">
+                  <img class="w-64" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/a17abde8d83650a582a28432/users-with-speech-bubbles-vector_53876-82250.jpg" alt="" />
+                  <div class="mt-4 text-gray-600 text-center">
+                    <h1 class="text-xl font-bold">Communications</h1>
+                    <p class="mt-4 text-gray-600">Pretium lectus quam id leo in vitae turpis. Mattis pellentesque id nibh tortor id.</p>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
+                <div class="w-sm">
+                  <img class="w-64" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/3b242447f922540fbe750cab/fdf.jpg" alt="" />
+                  <div class="mt-4 text-gray-600 text-center">
+                    <h1 class="text-xl font-bold">Inspired Design</h1>
+                    <p class="mt-4 text-gray-600">Nunc consequat interdum varius sit amet mattis vulputate enim nulla. Risus feugiat.</p>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
+                <div class="w-sm">
+                  <img class="w-64" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/8cc47b39e719570b996d9879/dsds.jpg" alt="" />
+                  <div class="mt-4 text-gray-600 text-center">
+                    <h1 class="text-xl font-bold">Happy Customers</h1>
+                    <p class="mt-4 text-gray-600">Nisl purus in mollis nunc sed id semper. Rhoncus aenean vel elit scelerisque mauris.</p>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <h3 class="mt-8 text-center text-5xl text-gray-600 font-bold">Software</h3>
+
+            <div class="md:flex md:justify-center md:space-x-8 md:px-14">
+              
+              <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
+                <div class="w-sm">
+                  <img class="w-64" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/a17abde8d83650a582a28432/users-with-speech-bubbles-vector_53876-82250.jpg" alt="" />
+                  <div class="mt-4 text-gray-600 text-center">
+                    <h1 class="text-xl font-bold">Communications</h1>
+                    <p class="mt-4 text-gray-600">Pretium lectus quam id leo in vitae turpis. Mattis pellentesque id nibh tortor id.</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
+                <div class="w-sm">
+                  <img class="w-64" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/3b242447f922540fbe750cab/fdf.jpg" alt="" />
+                  <div class="mt-4 text-gray-600 text-center">
+                    <h1 class="text-xl font-bold">Inspired Design</h1>
+                    <p class="mt-4 text-gray-600">Nunc consequat interdum varius sit amet mattis vulputate enim nulla. Risus feugiat.</p>
+                    
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-16 py-4 px-4 bg-whit w-72 bg-white rounded-xl shadow-lg hover:shadow-xl transform hover:scale-110 transition duration-500 mx-auto md:mx-0">
+                <div class="w-sm">
+                  <img class="w-64" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/8cc47b39e719570b996d9879/dsds.jpg" alt="" />
+                  <div class="mt-4 text-gray-600 text-center">
+                    <h1 class="text-xl font-bold">Happy Customers</h1>
+                    <p class="mt-4 text-gray-600">Nisl purus in mollis nunc sed id semper. Rhoncus aenean vel elit scelerisque mauris.</p>
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     )

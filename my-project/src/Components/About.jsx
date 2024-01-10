45bc62e97e77f68b5ab0089e90327cfc536d0da0
JuanRamonL <78512252+JuanRamonL@users.noreply.github.com>
2024-01-10 00:52:37 -0300

    componentes base

--- my-project/src/Components/About.jsx
+++ my-project/src/Components/About.jsx
@@ -1,12 +1,12 @@
 function About() {
   return (
     <>
-      <div class="grid lg:grid-cols-2 place-items-center  pb-8 ">
+      <div class="grid lg:grid-cols-2 place-items-center ">
         <div class=" md:order-1 hidden md:block">
           <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="" />
         </div>
-        <div>
-          <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight">
+        <div className="px-8">
+          <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold lg:tracking-tight ">
             Marketing website done with Astro
           </h1>
           <p class="text-lg mt-4 text-slate-600 max-w-xl">
